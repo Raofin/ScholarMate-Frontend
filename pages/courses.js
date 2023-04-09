692c03ad336d9f60d4a8c7992267b1105ae22d54
Zaid Amin <zaidaminraofin@gmail.com>
2023-04-10 02:56:57 +0600

    Add course and department details

--- pages/courses.js
+++ pages/courses.js
@@ -1,11 +1,43 @@
-import { Container } from 'react-bootstrap';
+import axios from 'axios';
+import { API_ENDPOINT } from './config';
 
-function CoursePage() {
-  return (
-    <Container className="mt-4 w-20 p-3 d-flex justify-content-center">
-      <h1>Courses</h1>
-    </Container>
-  );
+export async function getServerSideProps(context) {
+  const response = await axios.get(`${ API_ENDPOINT }/courses`);
+  const courses = response.data;
+
+  return { props: { courses } };
 }
 
-export default CoursePage;
+export default function CourseTable({ courses }) {
+  return (
+    <div className="container mx-auto">
+      <h1 className="my-4">Course List</h1>
+      <table className="table table-striped table-hover">
+        <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Code</th>
+          <th>Credit</th>
+          <th>Department</th>
+          <th>Registrar</th>
+          <th>Action</th>
+        </tr>
+        </thead>
+        <tbody>
+        { courses.map((course) => (
+          <tr key={ course.id }>
+            <td>{ course.id }</td>
+            <td>{ course.name }</td>
+            <td>{ course.code }</td>
+            <td>{ course.credit }</td>
+            <td>{ course.department.name }</td>
+            <td>{ course.registrar.name }</td>
+            <td><a href={ `/CourseDetails?id=${ course.id }` } className="btn btn-primary">View Details</a></td>
+          </tr>
+        )) }
+        </tbody>
+      </table>
+    </div>
+  );
+}
--- pages/departments.js
+++ pages/departments.js
@@ -1,11 +1,35 @@
-import { Container } from 'react-bootstrap';
+import axios from 'axios';
+import { API_ENDPOINT } from './config';
 
-function DepartmentPage() {
-  return (
-    <Container className="mt-4 w-20 p-3 d-flex justify-content-center">
-      <h1>Departments</h1>
-    </Container>
-  );
+export async function getServerSideProps(context) {
+  const response = await axios.get(`${ API_ENDPOINT }/departments`);
+  const departments = response.data;
+
+  return { props: { departments } };
 }
 
-export default DepartmentPage;
+export default function DepartmentTable({ departments }) {
+  return (
+    <div className="container mx-auto">
+      <h1 className="my-4">Department List</h1>
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Department Head</th>
+          </tr>
+        </thead>
+        <tbody>
+          { departments.map((department) => (
+            <tr key={ department.id }>
+              <td>{ department.name }</td>
+              <td>{ department.description }</td>
+              <td>{ department.head.name }</td>
+            </tr>
+          )) }
+        </tbody>
+      </table>
+    </div>
+  );
+}
