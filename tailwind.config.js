5bd449db46f57d65c6f1879bd25d9a86efe11d7a
Zaid Amin <zaidaminraofin@gmail.com>
2023-04-25 22:36:39 +0600

    Add Tailwind-CSS

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
