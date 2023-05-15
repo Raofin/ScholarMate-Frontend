bac88e5bd6c6221e038b3461d760a60c1681873f
Raofin <hello@raofin.net>
2023-05-15 12:19:15 +0600

    add img

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function HomePage() {
   return (
-    <div className="fixed top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 drop-shadow-lg">
-      <img src="/logo.svg" alt="ScholarMate logo" width={500} />
+    <div className=" drop-shadow-lg">
+      <img src="/logo.svg" className="ml-auto mr-auto" alt="ScholarMate logo" width={400} />
       <h1 className="text-center text-4xl font-black text-white">Welcome to ScholarMate!</h1>
     </div>
   );
